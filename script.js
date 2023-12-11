a48f0d8a71e8ea5e873ddea9d65153115d94ab32
HidekiMichi <105813974+HidekiMichi@users.noreply.github.com>
2023-12-11 20:16:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 const makeChange = (c) => {
   // your name here
+	
+	let q=Math.floor((c/25));
+	c=c-(25*q);
+	let d=Math.floor((c/10));
+	c=c-(10*d);
+	let n=Math.floor((c/5));
+	c=c-(5*n);
+	return {"q": q, "d": d, "n": n, "p": c };
 };
 
 // Do not the change the code below
 const c = prompt("Enter c: ");
 alert(JSON.stringify(makeChange(c)));
+
